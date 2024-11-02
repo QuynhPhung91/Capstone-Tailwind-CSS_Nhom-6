28ca554c3220414324612f5dc107a3146b087f79
Quynh Phung <139933051+QuynhPhung91@users.noreply.github.com>
2024-11-02 10:31:29 +0700

    add file index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
-        'jost': ['Jost', 'sans-serif'],
-        'nunito': ['Nunito', 'sans-serif'],
+        jost: ["Jost", "sans-serif"],
+        nunito: ["Nunito", "sans-serif"],
+      },
+      backdropBlur: {
+        main: "#fff",
+        warm: "fff0e5",
       },
     },
   },
-  plugins: [
-    
-  ],
-}
+
+  plugins: [require("flowbite/plugin")],
+};
